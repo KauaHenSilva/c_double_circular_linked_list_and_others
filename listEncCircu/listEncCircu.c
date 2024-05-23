4f72ef98c4ff69208b1688e5f37a64bb84ae4abb
WalisonWeudes <weudesw0@gmail.com>
2024-05-22 22:35:21 -0300

    arodenar lista

--- listEncCircu/listEncCircu.c
+++ listEncCircu/listEncCircu.c
@@ -1,10 +1,36 @@
 #include "listEncCircu.h"
 #include <stdio.h>
 
-Elemento *addInicio(Elemento *l){
-    Elemento *novo = (Elemento*) malloc(sizeof(Elemento));
+Elemento *addOrdeandoCrescente(Elemento *l){
+    Elemento *novo=(Elemento*)malloc(sizeof(Elemento));
     scanf("%d", &novo->info);
+    novo->prox=NULL;
+
+   if(l == NULL){
+    l = novo;
+    novo->prox = novo;
+   }
+   Elemento *aux = l;
+   if (novo->info <= l->info)
+   {
+    while (aux->prox != l )
+    {
+      aux= aux->prox;
+    }
+    aux->prox = novo;
     novo->prox = l;
-    return novo;
+    
+   }
+   
+   while(aux->prox != l && aux->prox->info < novo->info)
+   {
+      aux= aux->prox;
+   }
+    novo->prox = aux->prox;
+    aux->prox = novo;
+    return l;
+}
+void addFinal(Elemento *l){
+
 
 }
