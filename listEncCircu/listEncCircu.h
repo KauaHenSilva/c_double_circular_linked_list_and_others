2f42c1661988655b44ddc6c67c63fe8773421225
WalisonWeudes <weudesw0@gmail.com>
2024-05-22 21:07:10 -0300

    addlist

--- listEncCircu/listEncCircu.h
+++ listEncCircu/listEncCircu.h
@@ -1,2 +1,20 @@
 
-void helloWord();
+#ifndef LISTA_H
+#define LISTA_H
+
+
+typedef struct elemento{
+    int info;
+    struct  elemento *prox;
+}Elemento;
+
+Elemento *criaLista(); 
+Elemento *addInicio(Elemento *l); 
+void addFinal(Elemento *l); 
+Elemento *addOrdeandoCrescente(Elemento *l); 
+Elemento *removerElemento(Elemento *l); 
+int tamanhoDaLista(Elemento *l); 
+void mostrarLista(Elemento *l); 
+void exibicaomenu(); 
+
+#endif 
